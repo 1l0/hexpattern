d27d510777e847988cb7eee992d7516f2055d313
1l0 <rh.plus001@gmail.com>
2024-06-07 23:17:56 +0900

    ongoing, transparent pointer

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"d81edf635a9f47fd8b8dd1dc94afdb3eaffe51
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2227495824"
+    serviceWorkerVersion: "2499945489"
   }
 });
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,13 +2,15 @@ import 'dart:math';
 
 import 'package:flutter/material.dart' hide Card;
 
+import 'package:transparent_pointer/transparent_pointer.dart';
 // import 'package:google_fonts/google_fonts.dart';
 
 import 'card.dart';
 
 const sampleText =
     "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', \nmaking it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
-const columnWidth = 600;
+const contentColumnWidth = 600.0;
+const leftColumnWidth = 150.0;
 
 void main() {
   runApp(const MyApp());
@@ -56,145 +58,186 @@ class _MockPageState extends State<MockPage> {
 
   @override
   Widget build(BuildContext context) {
-    final isDarkMode =
-        MediaQuery.of(context).platformBrightness == Brightness.dark;
-    final logo = Padding(
-      padding: const EdgeInsets.symmetric(vertical: 35.0),
-      child: Image(
-        image: isDarkMode
-            ? const AssetImage('images/nosuta_dark.png')
-            : const AssetImage('images/nosuta_light.png'),
-        fit: BoxFit.fitHeight,
-        height: 20,
-      ),
-    );
+    // final isDarkMode =
+    //     MediaQuery.of(context).platformBrightness == Brightness.dark;
+    // final logo = Padding(
+    //   padding: const EdgeInsets.symmetric(vertical: 35.0),
+    //   child: Image(
+    //     image: isDarkMode
+    //         ? const AssetImage('images/nosuta_dark.png')
+    //         : const AssetImage('images/nosuta_light.png'),
+    //     fit: BoxFit.fitHeight,
+    //     height: 20,
+    //   ),
+    // );
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: LayoutBuilder(builder: (context, constraints) {
-        return CustomScrollView(
-          controller: scrollController,
-          slivers: [
-            SliverAppBar(
-              elevation: 0,
-              scrolledUnderElevation: 0,
-              title: GestureDetector(
-                onTap: scrollToTop,
-                child: logo,
-              ),
-              floating: true,
+        final contentColumnPadding = constraints.maxWidth > contentColumnWidth
+            ? max(constraints.maxWidth - contentColumnWidth, 0.0) / 2.0
+            : 0.0;
+        final leftColumnRemain =
+            (constraints.maxWidth - contentColumnWidth) / 2.0 - leftColumnWidth;
+        final leftColumnPadding =
+            leftColumnRemain > 0.0 ? leftColumnRemain : 0.0;
+        return Stack(
+          children: [
+            CustomScrollView(
+              controller: scrollController,
+              slivers: [
+                SliverAppBar(
+                  backgroundColor:
+                      Theme.of(context).colorScheme.surface.withAlpha(217),
+                  elevation: 0,
+                  scrolledUnderElevation: 0,
+                  title: GestureDetector(
+                    onTap: scrollToTop,
+                    //   child: logo,
+                    child: Text('Timeline'),
+                  ),
+                  floating: true,
+                ),
+                SliverPadding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: contentColumnPadding),
+                  sliver: const SliverList(
+                    delegate: SliverChildListDelegate.fixed(
+                      [
+                        Card(
+                          top: true,
+                          name: 'reishisaza',
+                          pubkeyHex:
+                              '3c7d12a6c2f71fe9ca2527216f529a137bb0f2eb018b18f30003933b9532013e',
+                          imageUrl:
+                              'https://image.nostr.build/nostr.build_011c3ed72e650b3a46e4d8cc78bdebbe0d2267ff18ff534380c501adba07db7b.png',
+                          text: 'テスト　テスト。',
+                        ),
+                        Card(
+                          name: 'fiatjaf',
+                          pubkeyHex:
+                              '3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d',
+                          imageUrl: 'https://fiatjaf.com/static/favicon.jpg',
+                          text:
+                              '@sepehr does osty.dev come with built-in components?',
+                        ),
+                        Card(
+                          name: 'jack',
+                          pubkeyHex:
+                              '82341f882b6eabcd2ba7f1ef90aad961cf074af15b9ef44a09f9d2a8fbfbe6a2',
+                          imageUrl:
+                              'https://pfp.nostr.build/nostr.build_6b9909bccf0f4fdaf7aacd9bc01e4ce70dab86f7d90395f2ce925e6ea06ed7cd.jpeg',
+                          text: 'yes',
+                        ),
+                        Card(
+                          name: 'サンプルユーザー🥹サンプルユーザー サンプルユーザー サンプルユーザ',
+                          // name: 'サンプルユーザー🥹',
+                          pubkeyHex:
+                              'ae6df144a3c0f76abf304a786663b2e7d2a14942e08e7ddc04f7ebff0d7add2a',
+                          imageUrl:
+                              'https://cdn.zebedee.io/uploads/3dd3eab9-80db-4bf3-b02e-40c1a53df2d3_0c94b03c-af87-465c-ac9c-5b43c18e6351.png',
+                          text: sampleText,
+                        ),
+                        Card(
+                          name: 'reishisaza',
+                          pubkeyHex:
+                              '3c7d12a6c2f71fe9ca2527216f529a137bb0f2eb018b18f30003933b9532013e',
+                          imageUrl:
+                              'https://image.nostr.build/nostr.build_011c3ed72e650b3a46e4d8cc78bdebbe0d2267ff18ff534380c501adba07db7b.png',
+                          text: 'テスト　テスト。',
+                        ),
+                        Card(
+                          name: 'fiatjaf',
+                          pubkeyHex:
+                              '3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d',
+                          imageUrl: 'https://fiatjaf.com/static/favicon.jpg',
+                          text:
+                              '@sepehr does osty.dev come with built-in components?',
+                        ),
+                        Card(
+                          name: 'jack',
+                          pubkeyHex:
+                              '82341f882b6eabcd2ba7f1ef90aad961cf074af15b9ef44a09f9d2a8fbfbe6a2',
+                          imageUrl:
+                              'https://pfp.nostr.build/nostr.build_6b9909bccf0f4fdaf7aacd9bc01e4ce70dab86f7d90395f2ce925e6ea06ed7cd.jpeg',
+                          text: 'yes',
+                        ),
+                        Card(
+                          name: 'サンプルユーザー🥹サンプルユーザー サンプルユーザー サンプルユーザ',
+                          // name: 'サンプルユーザー🥹',
+                          pubkeyHex:
+                              'ae6df144a3c0f76abf304a786663b2e7d2a14942e08e7ddc04f7ebff0d7add2a',
+                          imageUrl:
+                              'https://cdn.zebedee.io/uploads/3dd3eab9-80db-4bf3-b02e-40c1a53df2d3_0c94b03c-af87-465c-ac9c-5b43c18e6351.png',
+                          text: sampleText,
+                        ),
+                        Card(
+                          name: 'reishisaza',
+                          pubkeyHex:
+                              '3c7d12a6c2f71fe9ca2527216f529a137bb0f2eb018b18f30003933b9532013e',
+                          imageUrl:
+                              'https://image.nostr.build/nostr.build_011c3ed72e650b3a46e4d8cc78bdebbe0d2267ff18ff534380c501adba07db7b.png',
+                          text: 'テスト　テスト。',
+                        ),
+                        Card(
+                          name: 'fiatjaf',
+                          pubkeyHex:
+                              '3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d',
+                          imageUrl: 'https://fiatjaf.com/static/favicon.jpg',
+                          text:
+                              '@sepehr does osty.dev come with built-in components?',
+                        ),
+                        Card(
+                          name: 'jack',
+                          pubkeyHex:
+                              '82341f882b6eabcd2ba7f1ef90aad961cf074af15b9ef44a09f9d2a8fbfbe6a2',
+                          imageUrl:
+                              'https://pfp.nostr.build/nostr.build_6b9909bccf0f4fdaf7aacd9bc01e4ce70dab86f7d90395f2ce925e6ea06ed7cd.jpeg',
+                          text: 'yes',
+                        ),
+                        Card(
+                          name: 'サンプルユーザー🥹サンプルユーザー サンプルユーザー サンプルユーザ',
+                          // name: 'サンプルユーザー🥹',
+                          pubkeyHex:
+                              'ae6df144a3c0f76abf304a786663b2e7d2a14942e08e7ddc04f7ebff0d7add2a',
+                          imageUrl:
+                              'https://cdn.zebedee.io/uploads/3dd3eab9-80db-4bf3-b02e-40c1a53df2d3_0c94b03c-af87-465c-ac9c-5b43c18e6351.png',
+                          text: sampleText,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-            SliverPadding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: constraints.maxWidth > columnWidth
-                      ? max(constraints.maxWidth - columnWidth, 0) / 2
-                      : 0),
-              sliver: const SliverList(
-                delegate: SliverChildListDelegate.fixed(
-                  [
-                    Card(
-                      top: true,
-                      name: 'reishisaza',
-                      pubkeyHex:
-                          '3c7d12a6c2f71fe9ca2527216f529a137bb0f2eb018b18f30003933b9532013e',
-                      imageUrl:
-                          'https://image.nostr.build/nostr.build_011c3ed72e650b3a46e4d8cc78bdebbe0d2267ff18ff534380c501adba07db7b.png',
-                      text: 'テスト　テスト。',
+            if (leftColumnPadding > 0)
+              TransparentPointer(
+                child: Padding(
+                  padding: EdgeInsets.only(left: leftColumnPadding),
+                  child: Container(
+                    color: Theme.of(context).colorScheme.surfaceBright,
+                    width: leftColumnWidth,
+                    height: double.infinity,
+                    child: NavigationRail(
+                      extended: true,
+                      destinations: [
+                        NavigationRailDestination(
+                          icon: Icon(Icons.timeline),
+                          label: Text('Timeline'),
+                        ),
+                        NavigationRailDestination(
+                          icon: Icon(Icons.timeline),
+                          label: Text('Timeline'),
+                        ),
+                        NavigationRailDestination(
+                          icon: Icon(Icons.timeline),
+                          label: Text('Timeline'),
+                        ),
+                      ],
+                      selectedIndex: 0,
                     ),
-                    Card(
-                      name: 'fiatjaf',
-                      pubkeyHex:
-                          '3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d',
-                      imageUrl: 'https://fiatjaf.com/static/favicon.jpg',
-                      text:
-                          '@sepehr does osty.dev come with built-in components?',
-                    ),
-                    Card(
-                      name: 'jack',
-                      pubkeyHex:
-                          '82341f882b6eabcd2ba7f1ef90aad961cf074af15b9ef44a09f9d2a8fbfbe6a2',
-                      imageUrl:
-                          'https://pfp.nostr.build/nostr.build_6b9909bccf0f4fdaf7aacd9bc01e4ce70dab86f7d90395f2ce925e6ea06ed7cd.jpeg',
-                      text: 'yes',
-                    ),
-                    Card(
-                      name: 'サンプルユーザー🥹サンプルユーザー サンプルユーザー サンプルユーザ',
-                      // name: 'サンプルユーザー🥹',
-                      pubkeyHex:
-                          'ae6df144a3c0f76abf304a786663b2e7d2a14942e08e7ddc04f7ebff0d7add2a',
-                      imageUrl:
-                          'https://cdn.zebedee.io/uploads/3dd3eab9-80db-4bf3-b02e-40c1a53df2d3_0c94b03c-af87-465c-ac9c-5b43c18e6351.png',
-                      text: sampleText,
-                    ),
-                    Card(
-                      name: 'reishisaza',
-                      pubkeyHex:
-                          '3c7d12a6c2f71fe9ca2527216f529a137bb0f2eb018b18f30003933b9532013e',
-                      imageUrl:
-                          'https://image.nostr.build/nostr.build_011c3ed72e650b3a46e4d8cc78bdebbe0d2267ff18ff534380c501adba07db7b.png',
-                      text: 'テスト　テスト。',
-                    ),
-                    Card(
-                      name: 'fiatjaf',
-                      pubkeyHex:
-                          '3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d',
-                      imageUrl: 'https://fiatjaf.com/static/favicon.jpg',
-                      text:
-                          '@sepehr does osty.dev come with built-in components?',
-                    ),
-                    Card(
-                      name: 'jack',
-                      pubkeyHex:
-                          '82341f882b6eabcd2ba7f1ef90aad961cf074af15b9ef44a09f9d2a8fbfbe6a2',
-                      imageUrl:
-                          'https://pfp.nostr.build/nostr.build_6b9909bccf0f4fdaf7aacd9bc01e4ce70dab86f7d90395f2ce925e6ea06ed7cd.jpeg',
-                      text: 'yes',
-                    ),
-                    Card(
-                      name: 'サンプルユーザー🥹サンプルユーザー サンプルユーザー サンプルユーザ',
-                      // name: 'サンプルユーザー🥹',
-                      pubkeyHex:
-                          'ae6df144a3c0f76abf304a786663b2e7d2a14942e08e7ddc04f7ebff0d7add2a',
-                      imageUrl:
-                          'https://cdn.zebedee.io/uploads/3dd3eab9-80db-4bf3-b02e-40c1a53df2d3_0c94b03c-af87-465c-ac9c-5b43c18e6351.png',
-                      text: sampleText,
-                    ),
-                    Card(
-                      name: 'reishisaza',
-                      pubkeyHex:
-                          '3c7d12a6c2f71fe9ca2527216f529a137bb0f2eb018b18f30003933b9532013e',
-                      imageUrl:
-                          'https://image.nostr.build/nostr.build_011c3ed72e650b3a46e4d8cc78bdebbe0d2267ff18ff534380c501adba07db7b.png',
-                      text: 'テスト　テスト。',
-                    ),
-                    Card(
-                      name: 'fiatjaf',
-                      pubkeyHex:
-                          '3bf0c63fcb93463407af97a5e5ee64fa883d107ef9e558472c4eb9aaaefa459d',
-                      imageUrl: 'https://fiatjaf.com/static/favicon.jpg',
-                      text:
-                          '@sepehr does osty.dev come with built-in components?',
-                    ),
-                    Card(
-                      name: 'jack',
-                      pubkeyHex:
-                          '82341f882b6eabcd2ba7f1ef90aad961cf074af15b9ef44a09f9d2a8fbfbe6a2',
-                      imageUrl:
-                          'https://pfp.nostr.build/nostr.build_6b9909bccf0f4fdaf7aacd9bc01e4ce70dab86f7d90395f2ce925e6ea06ed7cd.jpeg',
-                      text: 'yes',
-                    ),
-                    Card(
-                      name: 'サンプルユーザー🥹サンプルユーザー サンプルユーザー サンプルユーザ',
-                      // name: 'サンプルユーザー🥹',
-                      pubkeyHex:
-                          'ae6df144a3c0f76abf304a786663b2e7d2a14942e08e7ddc04f7ebff0d7add2a',
-                      imageUrl:
-                          'https://cdn.zebedee.io/uploads/3dd3eab9-80db-4bf3-b02e-40c1a53df2d3_0c94b03c-af87-465c-ac9c-5b43c18e6351.png',
-                      text: sampleText,
-                    ),
-                  ],
+                  ),
                 ),
               ),
-            ),
           ],
         );
       }),
@@ -212,8 +255,10 @@ class NosutaTheme {
         tertiaryContainer: Color.fromARGB(255, 64, 64, 64),
         onTertiaryContainer: Color.fromARGB(255, 168, 168, 168),
         surface: Color.fromARGB(255, 29, 29, 29),
-        onSurface: Color.fromARGB(255, 207, 207, 207),
+        surfaceBright: Color.fromARGB(255, 41, 41, 41),
+        onSurface: Color.fromARGB(255, 216, 216, 216),
         onSurfaceVariant: Color.fromARGB(255, 137, 137, 137),
+        surfaceContainer: Color.fromARGB(255, 29, 29, 29),
         outlineVariant: Color.fromARGB(255, 66, 66, 66),
       ),
     );
@@ -232,6 +277,7 @@ class NosutaTheme {
         tertiaryContainer: Color.fromARGB(255, 247, 247, 247),
         onTertiaryContainer: Color.fromARGB(255, 124, 124, 124),
         surface: Color.fromARGB(255, 255, 255, 255),
+        surfaceBright: Color.fromARGB(255, 255, 255, 255),
         onSurface: Color.fromARGB(255, 0, 0, 0),
         onSurfaceVariant: Color.fromARGB(255, 128, 128, 128),
         surfaceContainer: Color.fromARGB(255, 255, 255, 255),
