6b8100d4eaea569d3e90082d2dfbd9774320a879
1l0 <rh.plus001@gmail.com>
2024-09-23 00:44:20 +0900

    update demo

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3a43be3422c0ff9236178793f1a7a0d9",
+const RESOURCES = {"flutter_bootstrap.js": "dabe28a26cf027cf460973c2259e2781",
 "version.json": "ff966ab969ba381b900e61629bfb9789",
 "index.html": "43539a92c1ac35dcbaf138d52737c1e9",
 "/": "43539a92c1ac35dcbaf138d52737c1e9",
-"main.dart.js": "1df5bd4e05de39e2da0c77aa85c4af73",
+"main.dart.js": "72fd9049f343b10d22125163c4ac09fc",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
